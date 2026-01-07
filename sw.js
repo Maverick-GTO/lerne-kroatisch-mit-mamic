41f79a5acafba67eeb660b229e3ca7f261c27f99
Maverick-GTO <irusanch@gmail.com>
2026-01-07 14:29:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'mamic-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './splash_logo.png'
+];
+
+// Installieren und Assets cachen
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Aktivieren
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Notwendig für die Installation: Fetch-Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
